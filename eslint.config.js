b269b9c180aab9789d846928753d5286c20bc6cc
grigory <savchenko.gv.work@gmail.com>
2025-12-04 16:25:06 +0400

    installed eslint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin';
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      'indent': ['error', 2],
+      '@stylistic/indent': ['error', 2],
+      // ...
+    }
+  }
+]);
