f4c03129593677e5d5693e6954d273d9e16b4738
grigory <savchenko.gv.work@gmail.com>
2025-12-10 11:59:59 +0400

    add brain-calc game

---
+++ bin/brain-calc.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import { greeting } from '../src/cli.js';
+
+const brainCalc = () => {
+  const userName = greeting();
+
+  console.log('What is the result of the expression?');
+
+  for (let i = 0; i < 3; i += 1) {
+    const num1 = parseInt(Math.random() * 51);
+    const num2 = parseInt(Math.random() * 51);
+    const obj = {
+      '+': num1 + num2,
+      '-': num1 - num2,
+      '*': num1 * num2,
+    }
+    const entries = Object.entries(obj);
+    const randomIndex = parseInt(Math.random() * 3);
+
+    const answer = readlineSync.question(`Question: ${num1} ${entries[randomIndex][0]} ${num2}\nYour answer: `);
+    const correctAnswer = entries[randomIndex][1];
+
+    if (correctAnswer === Number(answer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer. Correct answer was '${correctAnswer}'`);
+      console.log(`Let's try again, ${userName}`)
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}`);
+}
+brainCalc();
