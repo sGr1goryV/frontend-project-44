f42cc700aae842993cae76ebb83d0211a39375fe
grigory <savchenko.gv.work@gmail.com>
2026-01-03 19:06:51 +0400

    fix errors with Eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,17 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: {'@stylistic': stylistic},
+    rules: {'@stylistic/indent': ['error', 2]},
+    languageOptions: { globals: globals.browser } 
+  },
+
 ]);
+
+
