58135a930069600f7039a6c0e924ec1d4bf16477
grigory <savchenko.gv.work@gmail.com>
2026-01-05 16:40:44 +0400

    refactor: replaced Math.random to lodash.random

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import greeting from '../src/cli.js';
+import _ from 'lodash';
 
 const brainEven = () => {
     const userName = greeting();
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
     for (let i = 0; i < 3; i += 1) {
-        const randomNumber = Math.floor(Math.random() * 100)
+        const randomNumber = _.random(0, 50);
         const question = readlineSync.question(`Question: ${randomNumber}\nYour answer: `); //answer
         let correctAnswer;
         if (randomNumber % 2 === 0) {
